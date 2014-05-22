38d2efdb53a6d7440d0165bd462b76ff9d59ea70
Charles Collas <collas.charles@gmail.com>
2014-05-22 16:21:52 +0200

    Documentation + PHP wrapper class

--- README.md
+++ README.md
@@ -1,4 +1,155 @@
-mailjet-apiv3-php-simple
-========================
+# Mailjet PHP Wrapper for API v3
 
-Simple PHP wrapper for the Mailjet APIv3.
+## Introduction
+
+Provides a simple PHP library for the last version of the [MailJet API](http://dev.mailjet.com).
+The goal of this component is to simplify the usage of the MailJet API for PHP developers.
+
+### Prerequisites
+
+Make sure to have the following details:
+* Mailjet API Key
+* Mailjet API Secret Key
+* PHP
+* This PHP class
+
+## Installation
+
+First clone the repository
+```
+git clone https://github.com/mailjet/mailjet-apiv3-php-simple.git
+```
+
+Go into the mailjet-apiv3-php-simple folder and create an empty file named ```mailjetapi.php```
+```
+cd mailjet-apiv3-php-simple
+touch mailjetapi.php
+```
+
+You are now ready to go !
+
+## Usage
+
+First, you need to edit our php-mailjet-v3-simple.class.php file to assign your API keys to the variables ```$apiKey``` and ```$secretKey``` on lines 23 and 24 between simple quotes.
+You can find this keys in your Mailjet account here : https://app.mailjet.com/account/api_keys
+
+Then, in your mailjetapi.php file, you need to include our php class :
+
+```php
+include("php-mailjet-v3-simple.class.php");
+```
+**Be Careful :** Make sure that both mailjetapi.php and php-mailjet-v3-simple.class.php files are in the same folder to make this include work
+
+Now you can start working with the mailjetapi.php file by creating a new Mailjet object :
+```php
+$mj = new Mailjet();
+```
+
+This basically starts the engine. Now what you're going to do next depends on what you want to GET, POST, PUT or DELETE from the Mailjet servers through the API.
+Take a tour on the [Reference documentation](http://dev.mailjet.com/email-api/v3/apikey/) to see all the resources available.
+
+Next you will specify which resource to call this way (resource Contact in this example) with an array of parameters ```$params``` :
+```php
+$mj->contact($params);
+```
+**Info :** If you don't specify the method of the request in the array ```$params``` (see examples below), it will be a GET.
+
+## Examples
+Here are some function examples :
+
+- A function to print the list of your contacts :
+```php
+function listContacts()
+{
+	$mj = new Mailjet();
+	print_r($mj->contact());
+}
+```
+
+- A function to create a list with name ```$Lname``` :
+```php
+function createList($Lname) {
+    $mj = new Mailjet();
+    $params = array(
+    	"method" => "POST",
+    	"Name" => $Lname
+    );
+
+    echo "success - created list ".$Lname;
+
+    return $mj->contactslist($params);
+}
+```
+
+- A function to create a contact with email ```$Cemail``` :
+```php
+function createContact($Cemail) {
+  	$mj = new Mailjet();
+    $params = array(
+    	"method" => "POST",
+    	"Email" => $Cemail
+    );
+
+    echo "success - created contact ".$Cemail;
+
+    return $mj->contact($params);
+}
+```
+
+- A function to add the contact which ID is ```$contactID``` to the list which ID is ```$listID``` :
+```php
+function addContactToList($listID, $contactID) {
+	$mj = new Mailjet();
+    $params = array(
+    	"method" => "POST",
+    	"ContactID" => $contactID,
+    	"ListID" => $listID
+    );
+
+    echo "success - contact ".$contactID." added to the list ".$listID;
+
+    return $mj->listrecipient($params);
+}
+```
+
+- A function to print your profile information :
+```php
+function viewProfileInfo() {
+  	$mj = new Mailjet();
+	print_r($mj->myprofile());
+}
+```
+
+- A function to update the field ```AddressCity``` of your profile :
+```php
+function updateProfileInfo() {
+	$mj = new Mailjet();
+    $params = array(
+    	"method" => "PUT",
+    	"AddressCity" => "New York"
+    );
+
+    echo "success - field AddressCity changed";
+
+    return $mj->myprofile($params);
+}
+```
+
+- A function to delete the list which ID is ```$listID``` :
+```php
+function deleteList($listID) {
+	$mj = new Mailjet();
+    $params = array(
+    	"method" => "DELETE",
+    	"ID" => $listID
+    );
+
+    echo "success - deleted list ".$listID;
+
+    return $mj->contactslist($params);
+}
+```
+
+## Reporting issues
+
+Open an issue on github.
---
+++ php-mailjet-v3-simple.class.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+ * Mailjet Public API
+ *
+ * @package		API v0.3
+ * @author		Mailjet
+ * @link		http://api.mailjet.com/
+ *
+ */
+
+class Mailjet
+{
+    var $version = 'v3';
+
+    # Connect with https protocol
+    var $secure = true;
+
+   	# Mode debug ? 0 : none; 1 : errors only; 2 : all
+    var $debug = 0;
+
+    # Edit with your Mailjet API keys (you can find them here : https://app.mailjet.com/account/api_keys)
+    var $apiKey = '';
+    var $secretKey = '';
+    
+    # Constructor function
+    public function __construct($apiKey = false, $secretKey = false)
+    {
+        if ($apiKey)
+            $this->apiKey = $apiKey;
+        if ($secretKey)
+            $this->secretKey = $secretKey;
+        $this->apiUrl = (($this->secure) ? 'https' : 'http') . '://api.mailjet.com/v3/REST';
+    }
+    
+    public function __call($method, $args)
+    {
+        # Parameters array
+        $params  = (sizeof($args) > 0) ? $args[0] : array();
+
+        # Request method, GET by default
+        $request = isset($params["method"]) ? strtoupper($params["method"]) : 'GET';
+
+        # Request ID, empty by default
+        $id      = isset($params["ID"]) ? $params["ID"] : '';
+
+        # Unset useless params
+        if (isset($params["method"]))
+            unset($params["method"]);
+        if (isset($params["ID"]))
+            unset($params["ID"]);
+
+        # Make request
+        $result = $this->sendRequest($method, $params, $request, $id);
+
+        # Return result
+        $return = ($result === true) ? $this->_response : false;
+        if ($this->debug == 2 || ($this->debug == 1 && $return == false)) {
+            $this->debug();
+        }
+        return $return;
+    }
+
+    public function requestUrlBuilder($method, $params = array(), $request, $id)
+    {
+        foreach ($params as $key => $value) {
+            if ($request == "GET")
+                $query_string[$key] = $key . '=' . urlencode($value);
+        }
+        
+        $this->call_url = $this->apiUrl . '/' . $method;
+        if ($request == "VIEW" || $request == "DELETE" || $request == "PUT")
+            if ($id != '')
+                $this->call_url .= '/' . $id;
+        return $this->call_url;
+    }
+    
+    public function sendRequest($method = false, $params = array(), $request = "GET", $id = '')
+    {
+        # Method
+        $this->_method  = $method;
+        $this->_request = $request;
+        
+        # Build request URL
+        $url = $this->requestUrlBuilder($method, $params, $request, $id);
+        
+        # Set up and execute the curl process
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL, $url);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl_handle, CURLOPT_USERPWD, $this->apiKey . ':' . $this->secretKey);
+        
+        $this->_request_post = false;
+
+        if (($request == 'POST') || ($request == 'PUT')):
+            curl_setopt($curl_handle, CURLOPT_POST, count($params));
+            curl_setopt($curl_handle, CURLOPT_POSTFIELDS, json_encode($params));
+            curl_setopt($curl_handle, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+            ));
+            $this->_request_post = $params;
+        endif;
+
+        if ($request == 'DELETE') {
+            curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, "DELETE");
+        }
+
+        if ($request == 'PUT') {
+            error_log(json_encode($params));
+            curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, "PUT");
+        }
+
+        $buffer = curl_exec($curl_handle);
+        
+        if ($this->debug > 2)
+            var_dump($buffer);
+        
+        # Response code
+        $this->_response_code = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+        
+        # Close curl process
+        curl_close($curl_handle);
+        
+        # Return response
+        $this->_response = json_decode($buffer);
+        if ($request == 'POST')
+            return ($this->_response_code == 201) ? true : false;
+        if ($request == 'DELETE')
+            return ($this->_response_code == 204) ? true : false;
+        return ($this->_response_code == 200) ? true : false;
+        
+    }
+    
+    public function debug()
+    {
+        echo '<style type="text/css">';
+        echo '
+
+		#debugger {width: 100%; font-family: arial;}
+		#debugger table {padding: 0; margin: 0 0 20px; width: 100%; font-size: 11px; text-align: left;border-collapse: collapse;}
+		#debugger th, #debugger td {padding: 2px 4px;}
+		#debugger tr.h {background: #999; color: #fff;}
+		#debugger tr.Success {background:#90c306; color: #fff;}
+		#debugger tr.Error {background:#c30029 ; color: #fff;}
+		#debugger tr.Not-modified {background:orange ; color: #fff;}
+		#debugger th {width: 20%; vertical-align:top; padding-bottom: 8px;}
+
+		';
+        echo '</style>';
+        
+        echo '<div id="debugger">';
+        
+        if (isset($this->_response_code)):
+            if (($this->_response_code == 200) || ($this->_response_code == 201) || ($this->_response_code == 204)):
+                echo '<table>';
+                echo '<tr class="Success"><th>Success</th><td></td></tr>';
+                echo '<tr><th>Status code</th><td>' . $this->_response_code . '</td></tr>';
+                
+                if (isset($this->_response)):
+                    echo '<tr><th>Response</th><td><pre>' . utf8_decode(print_r($this->_response, 1)) . '</pre></td></tr>';
+                endif;
+                
+                echo '</table>';
+            elseif ($this->_response_code == 304):
+                echo '<table>';
+                echo '<tr class="Not-modified"><th>Error</th><td></td></tr>';
+                echo '<tr><th>Error no</th><td>' . $this->_response_code . '</td></tr>';
+                echo '<tr><th>Message</th><td>Not Modified</td></tr>';
+                echo '</table>';
+            else:
+                echo '<table>';
+                echo '<tr class="Error"><th>Error</th><td></td></tr>';
+                echo '<tr><th>Error no</th><td>' . $this->_response_code . '</td></tr>';
+                if (isset($this->_response)):
+                    if (is_array($this->_response) OR is_object($this->_response)):
+                        echo '<tr><th>Status</th><td><pre>' . print_r($this->_response, true) . '</pre></td></tr>';
+                    else:
+                        echo '<tr><th>Status</th><td><pre>' . $this->_response . '</pre></td></tr>';
+                    endif;
+                endif;
+                echo '</table>';
+            endif;
+        endif;
+        
+        $call_url = parse_url($this->call_url);
+        
+        echo '<table>';
+        echo '<tr class="h"><th>API config</th><td></td></tr>';
+        echo '<tr><th>Protocole</th><td>' . $call_url['scheme'] . '</td></tr>';
+        echo '<tr><th>Host</th><td>' . $call_url['host'] . '</td></tr>';
+        echo '<tr><th>Version</th><td>' . $this->version . '</td></tr>';
+        echo '</table>';
+        
+        echo '<table>';
+        echo '<tr class="h"><th>Call infos</th><td></td></tr>';
+        echo '<tr><th>Method</th><td>' . $this->_method . '</td></tr>';
+        echo '<tr><th>Request type</th><td>' . $this->_request . '</td></tr>';
+        echo '<tr><th>Get Arguments</th><td>';
+
+        if (isset($call_url['query'])) {
+            $args = explode("&", $call_url['query']);
+            foreach ($args as $arg) {
+                $arg = explode("=", $arg);
+                echo '' . $arg[0] . ' = <span style="color:#ff6e56;">' . $arg[1] . '</span><br/>';
+            }
+        }
+
+        echo '</td></tr>';
+
+        if ($this->_request_post) {
+            echo '<tr><th>Post Arguments</th><td>';
+            
+            foreach ($this->_request_post as $k => $v) {
+                echo $k . ' = <span style="color:#ff6e56;">' . $v . '</span><br/>';
+            }
+            
+            echo '</td></tr>';
+        }
+        
+        echo '<tr><th>Call url</th><td>' . $this->call_url . '</td></tr>';
+        echo '</table>';
+        
+        echo '</div>';
+    }
+}
