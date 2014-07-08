c848d2162619576cfbdd5a515e5a335b58995d52
Charles Collas <collas.charles@gmail.com>
2014-07-08 17:48:42 +0200

    Update contact.php

--- test/Mailjet/contact.php
+++ test/Mailjet/contact.php
@@ -9,19 +9,5 @@
         
         $this->assertEquals(200, $mj->_response_code);
     }
-    
-    public function testCreateContact()
-    {
-        $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
-        
-        $params = array(
-          "method" => "POST",
-          "Email" => "test@gmail.com"
-        );
-
-        $mj->contact($params);
-        
-        $this->assertEquals(201, $mj->_response_code);
-    }
   }
 ?>
