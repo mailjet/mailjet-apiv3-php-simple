c8f093569428320a08abb4db5f8422437b89cab6
Charles Collas <collas.charles@gmail.com>
2014-07-08 11:04:22 +0200

    Adding contact test

--- Tests/campaign.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function listContacts()
-	{
-    $mj = new Mailjet($_ENV['API_KEY'], $_ENV['API_SECRET_KEY']);
-    print_r($mj->contact());
-	}
-	assert_options(ASSERT_ACTIVE, 1);
-	assert(http_response_code(), 200);
-?>
