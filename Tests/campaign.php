31c32bfbe532a0945454a851033886cffd16eee2
Charles Collas <collas.charles@gmail.com>
2014-07-08 10:40:57 +0200

    Test list contacts

--- Tests/campaign.php
+++ Tests/campaign.php
@@ -1,5 +1,9 @@
 <?php
+	function listContacts()
+	{
+    $mj = new Mailjet($_ENV['API_KEY'], $_ENV['API_SECRET_KEY']);
+    print_r($mj->contact());
+	}
 	assert_options(ASSERT_ACTIVE, 1);
-	$test = 42;
-	assert('$test > 0');
+	assert(http_get_response_code(), 200);
 ?>
