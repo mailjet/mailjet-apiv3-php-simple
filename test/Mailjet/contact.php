11e3cb152deba0073f14fd4cc39bc9c0eb9fc992
Charles Collas <collas.charles@gmail.com>
2014-07-09 15:56:14 +0200

    Update contact.php : test needs to begin by test

--- test/Mailjet/contact.php
+++ test/Mailjet/contact.php
@@ -24,7 +24,7 @@
         $this->assertEquals(201, $mj->_response_code);
     }
 
-    public function deleteLastContactslist()
+    public function testdeleteLastContactslist()
     {
         $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
 
