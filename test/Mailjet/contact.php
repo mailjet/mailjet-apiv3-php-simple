7503d52439310fdb7f275346fe762bf069f3a138
Charles Collas <collas.charles@gmail.com>
2014-07-09 15:53:20 +0200

    Update contact.php

--- test/Mailjet/contact.php
+++ test/Mailjet/contact.php
@@ -24,28 +24,19 @@
         $this->assertEquals(201, $mj->_response_code);
     }
 
-    public function getLastContactslist()
+    public function deleteLastContactslist()
     {
         $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
 
         $mj->contactslist();
 
-        $this->assertEquals(200, $mj->_response_code);
-
-        return $mj->_response->Data[count($mj->_response->Data) - 1]->ID;
-    }
-
-    public function deleteLastContactslist()
-    {
-        $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
-
-        $lastid = getLastContactslist();
+        $lastid = $mj->_response->Data[count($mj->_response->Data) - 1]->ID;
 
         $params = array(
           "method" => "DELETE",
           "ID" => $lastid
         );
-        $mj->contactslist();
+        $mj->contactslist($params);
 
         $this->assertEquals(200, $mj->_response_code);
     }
