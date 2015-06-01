ee39328de539311d5dbe9e8a878d3c9e39b9a94b
Gaëtan Philippot <gaetan@mailjet.com>
2015-06-01 16:14:01 +0200

    Fixed partial compatibility with PHP v5.3. Added a condition to the json decoding process of the API response. Note that this fix also reveals that PHP v5.3 isn't compatible with the send API through this wrapper, due to a rounding bug in PHP. See here #54 and here #55.

--- src/Mailjet/php-mailjet-v3-simple.class.php
+++ src/Mailjet/php-mailjet-v3-simple.class.php
@@ -316,7 +316,17 @@ class Mailjet
         }
         else
         {
-            $this->_response = json_decode($buffer, false, 512, JSON_BIGINT_AS_STRING);
+            /*
+             *  This prevents the rounding error on 32 bits systems with PHP version >= 5.4
+             */
+            if (defined('JSON_BIGINT_AS_STRING'))
+            {
+                $this->_response = json_decode($buffer, false, 512, JSON_BIGINT_AS_STRING);
+            }
+            else
+            {   // PHP v <= 5.3.* doens't support the fourth parameter of json_decode
+                $this->_response = json_decode($buffer, false, 512);
+            }
         }
 
         if ($request == 'POST') {
