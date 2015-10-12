4a21fce261fff58924fe09ce7f096c1d5031b067
Guillaume Badi <gbadi@mailjet.com>
2015-10-12 16:38:56 +0200

    added simple test method to match call/url

--- test/Mailjet/contact.php
+++ test/Mailjet/contact.php
@@ -1,44 +1,56 @@
+
 <?php
+
+  require(__DIR__ . '/../../src/Mailjet/php-mailjet-v3-simple.class.php');
+
   class ContactsTest extends PHPUnit_Framework_TestCase
   {
+    public function __construct()
+    {
+      $this->mj = new Mailjet(getenv('MJ_APIKEY_PUBLIC'), getenv('MJ_APIKEY_PRIVATE'));
+    }
+
+    public function getCallUrl()
+    {
+        return $this->mj->call_url;
+    }
+
+    public function getPayload()
+    {
+      return $this->mj->_request_post;
+    }
+
+    public function getStatus()
+    {
+      return $this->mj->_response_code;
+    }
+
     public function testListContacts()
     {
-        $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
-
-        $mj->contact();
-
-        $this->assertEquals(200, $mj->_response_code);
+        $this->mj->contact();
+        $this->assertEquals(200, $this->getStatus());
     }
 
-    public function testCreateContactslist()
+    public function testPostSender()
     {
-        $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
-
-        $params = array(
-          "method" => "POST",
-          "Address" => "testcreatecontactslist@gmail.com",
-          "Name" => "testCreateContactslist"
-        );
-        $mj->contactslist($params);
-
-        $this->assertEquals(201, $mj->_response_code);
+      $this->mj->sender(array("method" => "POST", "email" => "gbadi@mailjet.com"));
+      $this->assertEquals("https://api.mailjet.com/v3/REST/sender", $this->getCallUrl());
+      $this->assertEquals(array("email" => "gbadi@mailjet.com"), $this->getPayload());
     }
 
-    public function testDeleteLastContactslist()
+    public function testGetSender()
     {
-        $mj = new Mailjet(getenv('API_KEY'), getenv('API_SECRET_KEY'));
+      $this->mj->sender();
+      $this->assertEquals("https://api.mailjet.com/v3/REST/sender", $this->getCallUrl());
+    }
 
-        $mj->contactslist();
-
-        $lastid = $mj->_response->Data[count($mj->_response->Data) - 1]->ID;
-
-        $params = array(
-          "method" => "DELETE",
-          "ID" => $lastid
-        );
-        $mj->contactslist($params);
-
-        $this->assertEquals(204, $mj->_response_code);
+    public function testContactID()
+    {
+      $this->mj->contact(array("method" => "GET", "id" => 2));
+      $this->assertEquals("https://api.mailjet.com/v3/REST/contact?id=2", $this->getCallUrl());
+      // TODO: lowercase id doesnt work
+      $this->mj->contact(array("method" => "VIEW", "ID" => 2));
+      $this->assertEquals("https://api.mailjet.com/v3/REST/contact/2", $this->getCallUrl());
     }
   }
 ?>
